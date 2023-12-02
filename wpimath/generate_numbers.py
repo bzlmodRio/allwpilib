c5aa86fe0567c38f966e967412b03ab9b0ffe3d2
PJ Reiniger <pj.reiniger@gmail.com>
2023-12-02 17:52:45 -0500

    Add bazel files

--- wpimath/generate_numbers.py
+++ wpimath/generate_numbers.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import argparse
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -26,8 +27,29 @@ def output(outPath, outfn, contents):
 def main():
     MAX_NUM = 20
 
-    dirname, _ = os.path.split(os.path.abspath(__file__))
-    cmake_binary_dir = sys.argv[1]
+    parser = argparse.ArgumentParser()
+
+    is_bazel = "RUNFILES_MANIFEST_FILE" in os.environ or "RUNFILES_DIR" in os.environ
+    if is_bazel:
+        parser.add_argument("--output_files", nargs="+")
+    else:
+        parser.add_argument("--cmake_binary_dir")
+    args = parser.parse_args()
+
+    if is_bazel:
+        dirname = "wpimath"
+
+        files_to_generate = args.output_files
+        base_name = os.path.dirname(files_to_generate[0])
+        generation_root = "/".join(base_name.split("/")[0:4]) + "/generated"
+
+        if len(files_to_generate) != (MAX_NUM + 1 + 1):
+            raise Exception(
+                f"Bazel is generating an unexpected number of files {len(files_to_generate)}"
+            )
+    else:
+        dirname, _ = os.path.split(os.path.abspath(__file__))
+        generation_root = f"{args.cmake_binary_dir}/generated/main"
 
     env = Environment(
         loader=FileSystemLoader(f"{dirname}/src/generate"),
@@ -36,14 +58,14 @@ def main():
     )
 
     template = env.get_template("GenericNumber.java.jinja")
-    rootPath = f"{cmake_binary_dir}/generated/main/java/edu/wpi/first/math/numbers"
+    rootPath = f"{generation_root}/java/edu/wpi/first/math/numbers"
 
     for i in range(MAX_NUM + 1):
         contents = template.render(num=i)
         output(rootPath, f"N{i}.java", contents)
 
     template = env.get_template("Nat.java.jinja")
-    rootPath = f"{cmake_binary_dir}/generated/main/java/edu/wpi/first/math"
+    rootPath = f"{generation_root}/java/edu/wpi/first/math"
     contents = template.render(nums=range(MAX_NUM + 1))
     output(rootPath, "Nat.java", contents)
 
