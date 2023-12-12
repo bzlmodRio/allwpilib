545849b947734750c0c9f5b54880998ecd929e4f
PJ Reiniger <pj.reiniger@gmail.com>
2023-12-12 01:29:43 -0500

    Even fixed the busted test

--- wpiutil/src/test/python/test_stacktrace.py
+++ wpiutil/src/test/python/test_stacktrace.py
@@ -2,9 +2,8 @@ import wpiutil
 
 
 def test_python_stack_trace():
-    pass
-    # st = wpiutil._wpiutil.getStackTrace(0)
-    # assert __file__ in st
+    st = wpiutil._wpiutil.getStackTrace(0)
+    assert __file__ in st
 
-    # st = wpiutil._wpiutil.getStackTraceDefault(0)
-    # assert __file__ not in st
+    st = wpiutil._wpiutil.getStackTraceDefault(0)
+    assert __file__ not in st
