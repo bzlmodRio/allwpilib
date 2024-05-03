0a62c4faa47a5cfae91812691b453f847c2482f9
PJ Reiniger <pj.reiniger@gmail.com>
2024-05-03 12:28:31 -0400

    Lint

--- wpilibcExamples/generate_bazel_files.py
+++ wpilibcExamples/generate_bazel_files.py
@@ -4,7 +4,7 @@ import json
 def load_foldernames(filename):
     output = []
 
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         data = json.load(f)
 
     for test_data in data:
@@ -12,14 +12,15 @@ def load_foldernames(filename):
 
     return sorted(set(output))
 
+
 def load_tests(filename):
     output = []
 
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         data = json.load(f)
 
     for test_data in data:
-        if test_data.get('hasunittests', False):
+        if test_data.get("hasunittests", False):
             output.append(test_data["foldername"])
 
     return sorted(set(output))
@@ -28,14 +29,20 @@ def load_tests(filename):
 def main():
     examples = load_foldernames("wpilibcExamples/src/main/cpp/examples/examples.json")
     commands = load_foldernames("wpilibcExamples/src/main/cpp/commands/commands.json")
-    templates = load_foldernames("wpilibcExamples/src/main/cpp/templates/templates.json")
+    templates = load_foldernames(
+        "wpilibcExamples/src/main/cpp/templates/templates.json"
+    )
     tests = load_tests("wpilibcExamples/src/main/cpp/examples/examples.json")
 
-    with open("wpilibcExamples/example_projects.bzl", 'w') as f:
-        f.write("EXAMPLE_FOLDERS = [\n    \"" + "\",\n    \"".join(examples) + "\",\n]\n\n")
-        f.write("COMMANDS_V2_FOLDERS = [\n    \"" + "\",\n    \"".join(commands) + "\",\n]\n\n")
-        f.write("TEMPLATES_FOLDERS = [\n    \"" + "\",\n    \"".join(templates) + "\",\n]\n\n")
-        f.write("TESTS_FOLDERS = [\n    \"" + "\",\n    \"".join(tests) + "\",\n]\n")
+    with open("wpilibcExamples/example_projects.bzl", "w") as f:
+        f.write('EXAMPLE_FOLDERS = [\n    "' + '",\n    "'.join(examples) + '",\n]\n\n')
+        f.write(
+            'COMMANDS_V2_FOLDERS = [\n    "' + '",\n    "'.join(commands) + '",\n]\n\n'
+        )
+        f.write(
+            'TEMPLATES_FOLDERS = [\n    "' + '",\n    "'.join(templates) + '",\n]\n\n'
+        )
+        f.write('TESTS_FOLDERS = [\n    "' + '",\n    "'.join(tests) + '",\n]\n')
 
 
 if __name__ == "__main__":
--- wpilibjExamples/generate_bazel_files.py
+++ wpilibjExamples/generate_bazel_files.py
@@ -4,7 +4,7 @@ import json
 def load_foldernames(filename):
     output = []
 
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         data = json.load(f)
 
     for test_data in data:
@@ -12,30 +12,45 @@ def load_foldernames(filename):
 
     return sorted(set(output))
 
+
 def load_tests(filename):
     output = []
 
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         data = json.load(f)
 
     for test_data in data:
-        if test_data.get('hasunittests', False):
+        if test_data.get("hasunittests", False):
             output.append(test_data["foldername"])
 
     return sorted(set(output))
 
 
 def main():
-    examples = load_foldernames("wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/examples.json")
-    commands = load_foldernames("wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/commands/commands.json")
-    templates = load_foldernames("wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/templates/templates.json")
-    tests = load_tests("wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/examples.json")
+    examples = load_foldernames(
+        "wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/examples.json"
+    )
+    commands = load_foldernames(
+        "wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/commands/commands.json"
+    )
+    templates = load_foldernames(
+        "wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/templates/templates.json"
+    )
+    tests = load_tests(
+        "wpilibjExamples/src/main/java/edu/wpi/first/wpilibj/examples/examples.json"
+    )
 
-    with open("wpilibjExamples/example_projects.bzl", 'w') as f:
-        f.write("EXAMPLES_FOLDERS = [\n    \"" + "\",\n    \"".join(examples) + "\",\n]\n\n")
-        f.write("COMMANDS_V2_FOLDERS = [\n    \"" + "\",\n    \"".join(commands) + "\",\n]\n\n")
-        f.write("TEMPLATES_FOLDERS = [\n    \"" + "\",\n    \"".join(templates) + "\",\n]\n\n")
-        f.write("TEST_FOLDERS = [\n    \"" + "\",\n    \"".join(tests) + "\",\n]\n")
+    with open("wpilibjExamples/example_projects.bzl", "w") as f:
+        f.write(
+            'EXAMPLES_FOLDERS = [\n    "' + '",\n    "'.join(examples) + '",\n]\n\n'
+        )
+        f.write(
+            'COMMANDS_V2_FOLDERS = [\n    "' + '",\n    "'.join(commands) + '",\n]\n\n'
+        )
+        f.write(
+            'TEMPLATES_FOLDERS = [\n    "' + '",\n    "'.join(templates) + '",\n]\n\n'
+        )
+        f.write('TEST_FOLDERS = [\n    "' + '",\n    "'.join(tests) + '",\n]\n')
 
 
 if __name__ == "__main__":
