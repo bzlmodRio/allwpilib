c427d2c25c02ae36681dd34109566613910ec1d0
PJ Reiniger <pj.reiniger@gmail.com>
2025-06-17 18:18:17 -0400

    Better

--- debug_compiler_args.py
+++ debug_compiler_args.py
@@ -3,9 +3,10 @@ import shutil
 import pathlib
 
 
-def dump_static():
-    gradle_file = r"C:\Users\PJ\git\wpilibsuite\allwpilib\wpiutil\build\tmp\createWpiutilBaseWindowsx86-64ReleaseStaticLibrary\options.txt"
-    bazel_file = r"C:\Users\PJ\git\wpilibsuite\allwpilib\bazel-bin\wpiutil\_wpiutil.lib-0.params"
+def dump_static(project, project_upper, operating_system, release_type):
+    gradle_file = os.path.join(project, "build", "tmp", f"generateExports{project_upper}{operating_system}{release_type}SharedLibrary/exports.def")
+    gradle_file = os.path.join(project, "build", "tmp", f"create{project_upper}Base{operating_system}{release_type}StaticLibrary/options.txt")
+    bazel_file = os.path.join("bazel-bin", project, "_" + project + ".lib-0.params")
 
     lines = []
     with open(gradle_file, 'r') as f:
@@ -24,35 +25,53 @@ def dump_static():
         f.write("\n".join(sorted(lines)))
 
 
-def main():
-    project = "wpiutil"
-    project_upper = project[0].upper() + project[1:]
-    cpp_file = "SmallVector"
-    release_type = "Release"
-    
-
-    gradle_file = os.path.join(project, "build", "tmp", f"compile{project_upper}BaseWindowsx86-64{release_type}StaticLibrary{project_upper}BaseWindowsx86-64ReleaseStaticLibrary{project_upper}WindowsCpp", "options.txt")
-    print(r"wpiutil\build\tmp\compileWpiutilBaseWindowsx86-64ReleaseStaticLibraryWpiutilBaseWindowsx86-64ReleaseStaticLibraryWpiutilWindowsCpp\options.txt")
-    print(gradle_file)
-    print(os.path.exists(gradle_file))
-
+def copy_compiler_flags(project, project_upper, operating_system, release_type, cpp_file):
+    if project == "wpiutil":
+        gradle_file = os.path.join(project, "build", "tmp", f"compile{project_upper}Base{operating_system}{release_type}StaticLibrary{project_upper}Base{operating_system}ReleaseStaticLibrary{project_upper}WindowsCpp", "options.txt")
+    else:
+        gradle_file = os.path.join(project, "build", "tmp", f"compile{project_upper}Base{operating_system}{release_type}StaticLibrary{project_upper}BaseCpp", "options.txt")
     bazel_file = os.path.join("bazel-bin", project, "_objs", "_" + project, cpp_file + ".obj.params")
-    print("bazel-bin\wpiutil\_objs\wpiutil\DataLog.obj.params")
-    print(bazel_file)
-    print(os.path.exists(bazel_file))
 
-
+    print(gradle_file)
+    print(r"wpilibc\build\tmp\compileWpilibcBaseWindowsx86-64ReleaseStaticLibraryWpilibcBaseCpp\options.txt")
     shutil.copy(gradle_file, "compare.gradle.txt")
     shutil.copy(bazel_file, "compare.bazel.txt")
 
 
-    gradle_file = os.path.join(project, "build", "tmp", "generateExportsWpiutilWindowsx86-64ReleaseSharedLibrary/exports.def")
-    bazel_file = f"bazel-out/x64_windows-opt/bin/{project}/{project}.gen.def"
+def copy_exports(project, project_upper, operating_system, release_type):
     
-    # shutil.copy(gradle_file, "def.gradle.txt")
-    # shutil.copy(bazel_file, "def.bazel.txt")
+    gradle_file = os.path.join(project, "build", "tmp", f"generateExports{project_upper}{operating_system}{release_type}SharedLibrary/exports.def")
+    bazel_file = f"bazel-bin/{project}/{project}.gen.def"
+    
+    shutil.copy(gradle_file, "def.gradle.txt")
 
-    dump_static()
+    shutil.copy(bazel_file, "def.bazel.txt")
+    os.chmod("def.bazel.txt", 0o777)
+
+
+def main():
+    project = "wpilibc"
+    cpp_file = "Solenoid"
+    
+    # project = "wpiutil"
+    # cpp_file = "SmallVector"
+    
+    # project = "wpinet"
+    # cpp_file = "WebServer"
+    
+    # project = "apriltag"
+    # cpp_file = "AprilTag"
+    
+
+    operating_system = "Windowsx86-64"
+    project_upper = project[0].upper() + project[1:]
+    release_type = "Release"
+    
+
+    copy_compiler_flags(project, project_upper, operating_system, release_type, cpp_file)
+    copy_exports(project, project_upper, operating_system, release_type)
+    dump_static(project, project_upper, operating_system, release_type)
+
 
 if __name__ == "__main__":
     main()
