75fad4a49963347c0fc682635f4e9ffb60d81fea
PJ Reiniger <pj.reiniger@gmail.com>
2025-06-28 15:24:29 -0400

    Add more stuff

--- wpilibc/src/test/python/test_notifier.py
+++ wpilibc/src/test/python/test_notifier.py
@@ -10,8 +10,10 @@ from wpilib import Notifier
 from wpilib.simulation import pauseTiming, restartTiming, resumeTiming, stepTiming
 
 # These tests fail because of race conditions in simulation
-if False:
-
+if True:
+    def test_dummy():
+        pass
+else:
     class AtomicInteger:
         def __init__(self) -> None:
             self.lock = threading.Lock()
