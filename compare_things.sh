cd2a7bdd709897feea2a099b1afd37b2bcc62713
PJ Reiniger <pj.reiniger@gmail.com>
2026-06-12 02:06:51 -0400

    More

--- compare_things.sh
+++ compare_things.sh
@@ -3,8 +3,3 @@
   --log_path=/tmp/local_compact.log \
   --output_path=/tmp/exec.log.txt \
   --output_path=/tmp/local_compact.log.txt
-
-mv /tmp/exec.log.txt .
-git add exec.log.txt
-
-# diff /tmp/exec.log.txt /tmp/local_compact.log.txt > diff.patch
