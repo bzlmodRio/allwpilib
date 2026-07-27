4dcf39a9a03f9a585842fdab6f654c3271d93552
PJ Reiniger <pj.reiniger@gmail.com>
2026-07-27 13:28:21 -0400

    [bazel][examples] Enforce tighter dependencies

--- wpilibcExamples/generate_bazel_files.py
+++ wpilibcExamples/generate_bazel_files.py
@@ -9,9 +9,24 @@ def load_foldernames(filename):
         data = json.load(f)
 
     for test_data in data:
-        output.append(test_data["foldername"])
+        vendordeps = []
 
-    return sorted(set(output))
+        for extra_vendordep in test_data.get("extravendordeps", []):
+            if extra_vendordep == "cameraserver":
+                vendordeps.append("//cameraserver")
+            elif extra_vendordep == "romi":
+                vendordeps.append("//romiVendordep")
+            elif extra_vendordep == "xrp":
+                vendordeps.append("//xrpVendordep")
+            else:
+                raise Exception(f"Unknown vendordep mapping for '{extra_vendordep}'")
+
+        command_version = test_data.get("commandversion", None)
+        if command_version == 2:
+            vendordeps.append("//commandsv2")
+        output.append((test_data["foldername"], sorted(vendordeps)))
+
+    return sorted(output)
 
 
 def load_tests(filename):
@@ -22,9 +37,19 @@ def load_tests(filename):
 
     for test_data in data:
         if test_data.get("hasunittests", False):
-            output.append(test_data["foldername"])
+            output.append((test_data["foldername"], []))
 
-    return sorted(set(output))
+    return sorted(output)
+
+
+def dump_chunk(constant_name, chunk_list):
+    output = constant_name + " = {\n"
+    for project_name, vendordeps in chunk_list:
+        output += (
+            f'    "{project_name}": [{", ".join(f'"{x}"' for x in vendordeps)}],\n'
+        )
+    output += "}\n"
+    return output
 
 
 def main():
@@ -42,24 +67,13 @@ def main():
         output_file = sys.argv[1]
 
     with open(output_file, "w") as f:
-        f.write('EXAMPLE_FOLDERS = [\n    "' + '",\n    "'.join(examples) + '",\n]\n\n')
-        f.write(
-            'COMMANDS_V2_FOLDERS = [\n    "' + '",\n    "'.join(commands) + '",\n]\n\n'
-        )
-        f.write('SNIPPET_FOLDERS = [\n    "' + '",\n    "'.join(snippets) + '",\n]\n\n')
-        f.write(
-            'TEMPLATE_FOLDERS = [\n    "' + '",\n    "'.join(templates) + '",\n]\n\n'
-        )
-        f.write(
-            'EXAMPLE_TESTS_FOLDERS = [\n    "'
-            + '",\n    "'.join(example_tests)
-            + '",\n]\n\n'
-        )
-        f.write(
-            'SNIPPET_TESTS_FOLDERS = [\n    "'
-            + '",\n    "'.join(snippet_tests)
-            + '",\n]\n'
-        )
+        f.write(dump_chunk("EXAMPLE_FOLDERS", examples) + "\n")
+        f.write(dump_chunk("COMMANDS_V2_FOLDERS", commands) + "\n")
+        f.write(dump_chunk("SNIPPET_FOLDERS", snippets) + "\n")
+        f.write(dump_chunk("TEMPLATE_FOLDERS", templates) + "\n")
+
+        f.write(dump_chunk("EXAMPLE_TESTS_FOLDERS", example_tests) + "\n")
+        f.write(dump_chunk("SNIPPET_TESTS_FOLDERS", snippet_tests))
 
 
 if __name__ == "__main__":
