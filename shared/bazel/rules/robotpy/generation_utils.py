d634366417a4308451a83a5e9aa5803e6e1c5c49
PJ Reiniger <pj.reiniger@gmail.com>
2026-08-07 15:38:31 -0400

    Relaced one thing

--- shared/bazel/rules/robotpy/generation_utils.py
+++ shared/bazel/rules/robotpy/generation_utils.py
@@ -1,38 +1,87 @@
+import dataclasses
+import typing
+import pathlib
+import tomli
+import functools
+import validobj.validation
+
+@dataclasses.dataclass
+class SubprojectConfig:
+    bazel_project_root: typing.Optional[str] = None
+    # is_native: typing.Optional[bool] = None
+    # project_base_name: typing.Optional[str] = ""
+    # copy_headers_target: typing.Optional[str] = None
+    # copy_headers_target_from_target: typing.Optional[str] = None
+    # local_extension_targets: typing.Optional[list[str]] = None
+    # pybind_target: typing.Optional[str] = None
+    # dynamic_dep: typing.Optional[str] = None
+
+    # def populate_defaults(self, project_name):
+    #     if self.is_native is None:
+    #         self.is_native = "native" in project_name
+
+    #     base_name = project_name.replace("robotpy-native-", "")
+    #     if self.is_native and self.copy_headers_target is None:
+    #         self.copy_headers_target = f"//{base_name}:{project_name}.copy_headers"
+            
+    #     if self.local_extension_targets is None:
+    #         self.local_extension_targets = [f"//{base_name}:{base_name}"]
+            
+    #     if self.pybind_target is None:
+    #         self.pybind_target = pybind_target = f"//{base_name}:{base_name}_pybind_library"
+
+    #     if self.dynamic_dep is None:
+    #         self.dynamic_dep = f"//{project_name}:shared/{project_name}"
+
+    #     if self.copy_headers_target_from_target is None:
+    #         self.copy_headers_target_from_target = f":robotpy-native-{project_name}.copy_headers"
+
+
+@dataclasses.dataclass
+class BazelTranslationConfig:
+    projects: typing.Dict[str, SubprojectConfig]
+
+    # def __post_init__(self):
+    #     for key in self.projects:
+    #         self.projects[key].populate_defaults(key)
+
+    # def get_copy_headers_target(self, project_name):
+    #     return self.projects[project_name].copy_headers_target
+
+    # def get_copy_headers_target_from_base_library(self, project_name):
+    #     return self.projects[project_name].copy_headers_target_from_target
+
+    # def get_local_extension_targets(self, project_name: str, include_pybind_target: bool):
+    #     output = self.projects[project_name].local_extension_targets
+    #     if include_pybind_target:
+    #         output.append(self.projects[project_name].pybind_target)
+
+    #     return output
+
+    # def get_dynamic_dep(self, project_name) -> typing.Optional[str]:
+    #     return self.projects[project_name].dynamic_dep
+
+
+
+_CONFIG_PATH = pathlib.Path(__file__).parent / "project_config.toml"
+
+@functools.lru_cache
+def load_config():
+    with open(_CONFIG_PATH, "rb") as fp:
+        toml_data = tomli.load(fp)
+
+    config = validobj.validation.parse_input(toml_data, BazelTranslationConfig)
+    return config
+
+
 def fixup_root_package_name(name):
-    if name == "wpihal":
-        return "hal"
-    if name == "wpilib":
-        return "wpilibc"
-    if name == "wpilog":
-        return "datalog"
-    if name == "xrp":
-        return "xrpVendordep"
-    if name == "romi":
-        return "romiVendordep"
-    if name == "pyntcore":
-        return "ntcore"
-    if name == "halsim-ws":
-        return "simulation/halsim_ws_core"
-    if name == "halsim_ws":
-        return "simulation/halsim_ws_core"
-    if name == "halsim_gui":
-        return "simulation/halsim_gui"
-    if name == "halsim_ds_socket":
-        return "simulation/halsim_ds_socket"
-    if name == "halsim_ws_server":
-        return "simulation/halsim_ws_server"
-    if name == "halsim_ws_client":
-        return "simulation/halsim_ws_client"
-    if name == "halsim_xrp":
-        return "simulation/halsim_xrp"
-    if name == "wpimath_test":
-        return "wpimath"
-    if name == "robotpy_apriltag":
-        return "apriltag"
-    if name == "robotpy_fields":
-        return "fields"
-    if name == "commands2":
-        return "commandsv2"
+    config = load_config()
+
+    if name in config.projects:
+        if config.projects[name].bazel_project_root:
+            return config.projects[name].bazel_project_root
+
+    print(f"Using default for {name}")
     return name
 
 
